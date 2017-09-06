c25ddefdeb1b09db545dbd11720aae1b23ece1f7
Sean <sgilmore@michael-sean.com>
2017-09-06 13:56:22 -0400

    added edit form and update databse routes, all of part 3

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -9,7 +9,7 @@ class CreaturesController < ApplicationController
   end
 
   def create
-    creature_params = params.require(:creature).permit(:name, :description)
+    # creature_params = params.require(:creature).permit(:name, :description)
     creature = Creature.new(creature_params)
     if creature.save
       redirect_to creature_path(creature)
@@ -19,5 +19,24 @@ class CreaturesController < ApplicationController
   def show
     creature_id = params[:id]
     @creature = Creature.find_by_id(creature_id)
+  end
+
+  def edit
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+  end
+
+  def update
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name,:description)
   end
 end
